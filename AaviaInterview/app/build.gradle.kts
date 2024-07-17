bd56811bd1c286e1f83e6feba77a921b7c9eb85a
Derril Lucci <dlucci@derrils-mbp.lan>
2024-07-16 20:13:59 -0400

    Setting up dependencies

--- AaviaInterview/app/build.gradle.kts
+++ AaviaInterview/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 }
 
 android {
@@ -50,7 +52,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,6 +60,14 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation)
+    implementation(libs.material)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.hiltAndroid)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
+    implementation(libs.kotlinxSerialization)
+    kapt(libs.hiltCompiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
